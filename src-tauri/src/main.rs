6b537c33d210b066418e4b4185ac02326b5f3642
akmot9 <avicocyprien@yahoo.com>
2024-07-09 16:54:25 +0200

    works

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::sync::{Arc, Mutex};
+
 mod commands;
 use commands::{get_matrice, set_recording};
 mod state;
@@ -9,7 +9,7 @@ use state::SonarState;
 
 fn main() {
     tauri::Builder::default()
-        .manage(Arc::new(Mutex::new(SonarState::new())))
+        .manage(SonarState::new())
         .invoke_handler(tauri::generate_handler![get_matrice,set_recording])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
